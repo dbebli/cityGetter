f73db9a70ff547ad449e0e5dcccb54f427cfa719
dbebli <beblidaniel@gmail.com>
2019-09-12 09:31:49 +0200

    some optimizations

--- api/api.php
+++ api/api.php
@@ -1,20 +1,22 @@
 <?php
 header("Access-Control-Allow-Origin:*");
 header("Content-Type:application/json");
-require "../inc/init.inc.php";
-require "../inc/methods.inc.php";
+require "inc/init.inc.php";
+require "Ville.php";
+
+$ville = new Ville($pdo);
 
 
 if (isset($_GET['search'])) {
-    $result = getByAttributes($pdo);
+    $result = $ville->getByAttributes($pdo);
     echo json_encode(["cities"=>$result]);
 }
 else{
     //at the beginning get all cities
-    $allCities = getCities($pdo);
-    $allMonths= getMonths($pdo);
-    $allActivite = getActivite($pdo);
-    $allContinent = getContinent($pdo);
+    $allCities =  $ville->getCities($pdo);
+    $allMonths=  $ville->getMonths($pdo);
+    $allActivite =  $ville->getActivite($pdo);
+    $allContinent =  $ville->getContinent($pdo);
     $tab=["cities"=>$allCities,"months"=>$allMonths,"activite"=>$allActivite,"continent"=>$allContinent];
   echo json_encode($tab);
 }
